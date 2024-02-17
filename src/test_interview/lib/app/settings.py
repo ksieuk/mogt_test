c62bb7e04103aa958a6fd005d91f3eb3fde7f939
ksieuk <iebebd3@gmail.com>
2024-02-17 21:14:42 +0300

    feat: [#3] proxy settings

--- src/test_interview/lib/app/settings.py
+++ src/test_interview/lib/app/settings.py
@@ -7,3 +7,4 @@ class Settings(pydantic_settings.BaseSettings):
     api: app_split_settings.ApiSettings = app_split_settings.ApiSettings()
     app: app_split_settings.AppSettings = app_split_settings.AppSettings()
     logger: app_split_settings.LoggingSettings = app_split_settings.LoggingSettings()
+    proxy: app_split_settings.ProxyBaseSettings = app_split_settings.ProxyBaseSettings()
--- src/test_interview/lib/app/split_settings/__init__.py
+++ src/test_interview/lib/app/split_settings/__init__.py
@@ -1,10 +1,12 @@
 from .api import *
 from .app import *
 from .logger import *
+from .proxy import *
 
 __all__ = [
     "ApiSettings",
     "AppSettings",
     "LoggingSettings",
+    "ProxyBaseSettings",
     "get_logging_config",
 ]
