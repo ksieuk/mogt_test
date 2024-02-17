43412938cdd85a5d7d96d81e6585ea373c37d285
ksieuk <iebebd3@gmail.com>
2024-02-17 23:41:39 +0300

    feat: [#3] test api from config

--- src/test_interview/tests/core/split_settings/api.py
+++ src/test_interview/tests/core/split_settings/api.py
@@ -1,5 +1,8 @@
+import typing
+
 import pydantic
 import pydantic_settings
+import yaml
 
 import lib.app.split_settings.utils as app_split_settings_utils
 
@@ -16,8 +19,24 @@ class ApiSettings(pydantic_settings.BaseSettings):
     host: str = "0.0.0.0"
     port: int = 8000
     headers: dict[str, str] = {"Content-Type": "application/json"}
+    use_config: bool = True
 
     @pydantic.computed_field
     @property
     def get_api_url(self) -> str:
         return f"{self.protocol}://{self.host}:{self.port}/api/v1"
+
+    @pydantic.model_validator(mode="before")
+    @classmethod
+    def load_from_config(cls, values: dict[str, typing.Any]) -> dict[str, typing.Any]:
+        config_path = app_split_settings_utils.BASE_PATH / "config.yaml"
+        if not values.get("use_config", True):
+            return values
+        if not config_path.exists():
+            return values
+
+        with open(config_path, encoding="utf-8") as file:
+            data = yaml.safe_load(file)
+        if "api_info" in data:
+            values.update(data["api_info"])
+        return values
